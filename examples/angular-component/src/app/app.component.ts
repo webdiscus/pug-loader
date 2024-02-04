bd9ce86a65f11cfa011c353d0e64a19e47af5c87
webdiscus <biodiscus@gmail.com>
2024-02-04 16:46:54 +0100

    chore: update dependencies in examples

--- examples/angular-component/src/app/app.component.ts
+++ examples/angular-component/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener } from '@angular/core';
 
+// TODO: fix passing url params as external data into pug
 const templateVars = '{"title": "pug template with variables", "count": ' + (777 + 111) + '}';
 
 @Component({
